eab7410b55ceab8d000b331ef82565285226a798
Alex Abatov <a.aliksant315@gmail.com>
2025-06-08 07:47:40 +0900

    feat: MongoDB connect logic

--- server.js
+++ server.js
@@ -1,51 +1,31 @@
-const express = require("express");
-const app = express();
 const http = require("http");
-const fs = require("fs");
 
-let user;
-fs.readFile("database/user.json", "utf-8", (err, data) => {
-  if (err) {
-    console.log("ERROR:", err);
-  } else {
-    user = JSON.parse(data);
+const mongodb = require("mongodb");
+const { toUnicode } = require("punycode");
+let db;
+const connectionString =
+  "mongodb+srv://maliksant33:WaWa5rpH5mr2QP6l@cluster0.lput7u2.mongodb.net/";
+
+mongodb.connect(
+  connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) {
+      console.log("ERROR on connection MongoDB");
+    } else {
+      console.log("MongoDB connection succeed");
+      module.exports = client;
+      const app = require("./app");
+      const server = http.createServer(app);
+      let PORT = 3000;
+      server.listen(PORT, function () {
+        console.log(
+          `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+        );
+      });
+    }
   }
-});
-
-//1: Kirish code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-//2: Session code
-//3: Views code
-
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-//4: Routing code
-
-// app.post("/create-item", (req, res) => {
-//   console.log(req.body);
-//   res.json({ test: "succes" });
-// });
-
-// app.post("/create-item", (req, res) => {
-//   // TODO: code with db
-// });
-
-app.get("/author", (req, res) => {
-  res.render("author", { user: user });
-});
-
-app.get("/", function (req, res) {
-  res.render("reja");
-});
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-  console.log(
-    `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
-  );
-});
+);
